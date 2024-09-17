91debee4352a0b6be7553cc18eb5aa0938c16ad8
matheustthh <163013250+matheustthh@users.noreply.github.com>
2024-09-17 08:03:27 -0300

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,9 @@
 var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Aparecida Nutricionista"
+
+var paulo = document.querySelector('#paciente1');
+var peso = paulo.querySelector('.info-peso').textContent;
+var altura = paulo.querySelector('.info-altura').textContent;
+var imc = peso / (altura*altura);
+document.querySelector('.info-imc').textContent = imc;
